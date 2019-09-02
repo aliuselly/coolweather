6ffcf3c59d8257ac27180af974c6b2d3b1ad90b4
aliuselly <1574626055@qq.com>
2019-09-02 18:05:18 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,7 +1,9 @@
 package com.coolweather.android;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -102,6 +104,14 @@ public class ChooseAreaFragment extends Fragment {
                 {
                     selectedCity = cityList.get(i);
                     queryCounties();
+                }
+                else if(currentLevel == LEVEL_COUNTY)
+                {
+                    String weatherId = countyList.get(i).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
